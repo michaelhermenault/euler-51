94b9859c333287a6047a52e5fcc35d3479897e1e
BishopofWales <mhermenault59@gmail.com>
2019-09-12 18:21:58 -0400

    Remove errant code, add comments

--- sieve.py
+++ sieve.py
@@ -1,21 +1,13 @@
 
 def sieve(num_list, prime_list):
+  #Remove all elements divisible by the last element in the prime list
   num_list = [element for element in num_list if element % prime_list[-1] != 0]
+  #Base case if all numbers have been elminated.
   if(len(num_list) == 0):
     return prime_list
+  #Otherwise append lowest number left in num list to the prime list
   prime_list.append(num_list.pop(0))
+  #Recurse
   return sieve(num_list, prime_list)
   
-#for element in range(3,100):
-#  print(element)
 print(sieve(range(3,121), [2]))
-
-def find_prime_family(ceiling):
-  primes = sieve(range(3,ceiling),[2])
-  for(prime in primes):
-    
-
-def all_families(num_elements):
-  if num_elements == 1:
-    return [[True],[False]]
-  [ [True] + element
